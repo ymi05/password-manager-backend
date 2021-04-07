3f7cd5be2dba4f2ca43913efe7366381e6fb3762
Youssef Itani <Youssef.itani@gmail.com>
2021-04-07 21:42:55 +0300

    Fixes

--- Scripts/Profile_Verify.sql
+++ Scripts/Profile_Verify.sql
@@ -1,4 +1,4 @@
-CREATE OR ALTER PROCEDURE [dbo].[Profile_Verify]
+CREATE OR ALTER   PROCEDURE [dbo].[Profile_Verify]
 (
 	@Profile_id INT
 	,@Verification_Code VARCHAR(6)
@@ -7,10 +7,9 @@ AS
 SET NOCOUNT ON;
 IF EXISTS (SELECT * FROM Verification_Code WHERE @Profile_id = profile_id AND @Verification_Code=code_value)
 	BEGIN
-		INSERT INTO Verfied_Profile (profile_id) VALUES (@Profile_id)
+		INSERT INTO Verified_Profile (profile_id) VALUES (@Profile_id)
 	SELECT 'You have been verified' [message] FOR JSON PATH;
 	END
 ELSE
 	SELECT 'Invalid verficiation code' [message] FOR JSON PATH;
 GO
-
